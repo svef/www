d3e2057718b03de480ab3f75f3011fcd6d1df782
Benedikt D Valdez <benediktvaldez@gmail.com>
2018-02-14 01:38:17 +0000

    feat: add & delete members

--- websites/members.svef.is/src/app/components/MemberList/MemberList.js
+++ websites/members.svef.is/src/app/components/MemberList/MemberList.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet'
 import Fuse from 'fuse.js'
 import throttle from 'lodash.throttle'
 
+import { Button } from '@svef/components'
 import { database } from 'app/firebase'
 
 import './MemberList.scss'
@@ -18,6 +19,8 @@ class Members extends Component {
     this.ref = database.ref('/felagar')
 
     this.handleChange = throttle(this.handleChange.bind(this), 150)
+    this.validateEmail = this.validateEmail.bind(this)
+    this.addNewMember = this.addNewMember.bind(this)
     this.renderPayerKt = this.renderPayerKt.bind(this)
   }
 
@@ -43,6 +46,45 @@ class Members extends Component {
     })
   }
 
+  validateEmail(value) {
+    return /(.+)@(.+)\.(.+)/.test(value).toString()
+  }
+
+  deleteMember(key) {
+    const member = this.state.members[key]
+    if (member && typeof window !== 'undefined') {
+      if (
+        window.confirm(
+          `Ertu VISS um að þú viljir eyða ${member.nafn ||
+            member.netfang ||
+            'þessum félaga'}? Aðgerðin er óafturkvæm!`
+        )
+      ) {
+        console.log('DELETING', member.nafn)
+
+        this.ref.update({ [`/${key}`]: null })
+      }
+    }
+  }
+
+  addNewMember() {
+    const newMember = {
+      kennitala: '',
+      kennitala_greidanda: '',
+      nafn: '',
+      netfang: '',
+      vinnustadur: '',
+    }
+
+    console.log('new member', newMember)
+
+    this.ref.push().set(newMember)
+
+    if (typeof window !== 'undefined') {
+      window.scrollTo(0, document.body.scrollHeight)
+    }
+  }
+
   renderPayerKt(kt) {
     return kt.charAt(0) > 3 ? (
       <p>
@@ -65,27 +107,36 @@ class Members extends Component {
   }
 
   render() {
-    const list = this.state.members.filter(
-      m =>
-        this.props.term === ''
-          ? true
-          : m.nafn.toLowerCase().includes(this.props.term) ||
-            m.netfang.toLowerCase().includes(this.props.term) ||
-            m.vinnustadur.toLowerCase().includes(this.props.term) ||
-            m.kennitala.toLowerCase().includes(this.props.term) ||
-            m.kennitala_greidanda.toLowerCase().includes(this.props.term)
-    )
+    const { term } = this.props
+    const { members } = this.state
+    const membersKeys = Object.keys(members)
+
+    const matchesTerm = member =>
+      term === ''
+        ? true
+        : member.nafn.toLowerCase().includes(term) ||
+          member.netfang.toLowerCase().includes(term) ||
+          member.vinnustadur.toLowerCase().includes(term) ||
+          member.kennitala.toLowerCase().includes(term) ||
+          member.kennitala_greidanda.toLowerCase().includes(term)
+
+    const list = membersKeys.filter(key => matchesTerm(members[key]))
     return (
       <div className="row MemberList">
         <div className="col-12">
           <div className="row title">
             <div className="col-10 offset-1">
               <p className="member_changes">
+                <Button theme="blue" onClick={this.addNewMember}>
+                  Bæta við félaga
+                </Button>
+                <br />
+                <br />
                 Gætið þess að allar breytingar eru varanlegar! Það er enginn
                 'Vista' takki, allt uppfærist jafnóðum og því er breytt.
                 <br />
-                Félagar: {this.props.term === '' ? '' : `${list.length}/`}
-                {this.state.members.length}
+                Félagar: {term === '' ? '' : `${list.length}/`}
+                {membersKeys.length}
               </p>
             </div>
             <div className="col-2 offset-1">
@@ -104,17 +155,18 @@ class Members extends Component {
               <p>Vinnustaður</p>
             </div>
           </div>
-          {list.map(
-            (member, i) =>
-              member.nafn && (
-                <div className="row member_row" key={`member-${i}`}>
+          {membersKeys.map(key => {
+            const member = members[key]
+            return (
+              matchesTerm(member) && (
+                <div className="row member_row" key={`member-${key}`}>
                   <div className="col-2 offset-1">
                     <input
                       type="text"
                       name="nafn"
                       value={member.nafn}
                       onChange={event =>
-                        this.handleChange(i, 'nafn', event.target.value)
+                        this.handleChange(key, 'nafn', event.target.value)
                       }
                     />
                   </div>
@@ -123,8 +175,9 @@ class Members extends Component {
                       type="text"
                       name="netfang"
                       value={member.netfang}
+                      valid={this.validateEmail(member.netfang)}
                       onChange={event =>
-                        this.handleChange(i, 'netfang', event.target.value)
+                        this.handleChange(key, 'netfang', event.target.value)
                       }
                     />
                   </div>
@@ -134,7 +187,7 @@ class Members extends Component {
                       name="kennitala"
                       value={member.kennitala}
                       onChange={event =>
-                        this.handleChange(i, 'kennitala', event.target.value)
+                        this.handleChange(key, 'kennitala', event.target.value)
                       }
                     />
                   </div>
@@ -145,7 +198,7 @@ class Members extends Component {
                       value={member.kennitala_greidanda}
                       onChange={event =>
                         this.handleChange(
-                          i,
+                          key,
                           'kennitala_greidanda',
                           event.target.value
                         )
@@ -158,13 +211,42 @@ class Members extends Component {
                       name="vinnustadur"
                       value={member.vinnustadur}
                       onChange={event =>
-                        this.handleChange(i, 'vinnustadur', event.target.value)
+                        this.handleChange(
+                          key,
+                          'vinnustadur',
+                          event.target.value
+                        )
                       }
                     />
                   </div>
+                  <div className="col-1">
+                    <p>
+                      <Button
+                        theme="action red"
+                        onClick={() => this.deleteMember(key)}
+                      >
+                        -
+                      </Button>
+                    </p>
+                  </div>
                 </div>
               )
-          )}
+            )
+          })}
+          <div className="row title">
+            <div className="col-10 offset-1">
+              <p className="member_changes">
+                <br />
+                Gætið þess að allar breytingar eru varanlegar! Það er enginn
+                'Vista' takki, allt uppfærist jafnóðum og því er breytt.
+                <br />
+                <br />
+                <Button theme="blue" onClick={this.addNewMember}>
+                  Bæta við félaga
+                </Button>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     )
